71593a53b45bb23adfd01f77df0144154e4bb12c
Willy Chang <ms70831@gmail.com>
2017-08-25 20:02:31 +0800

    create presenter test case

---
+++ app/src/test/java/com/devwilly/mvpex/presenter/MainPresenterTest.java
@@ -0,0 +1,31 @@
+package com.devwilly.mvpex.presenter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by Willy on 24/08/2017.
+ */
+public class MainPresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void onSaveButtonClick() throws Exception {
+    }
+
+    @Test
+    public void onDeleteButtonClick() throws Exception {
+    }
+
+}
